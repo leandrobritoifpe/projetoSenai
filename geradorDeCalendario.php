ab01eedda95ca2b538f0880b25f6a5b1ba27174b
Ets.areias <Ets.areias>
2016-08-24 12:20:27 -0300

    o programa sabe gerar calendario

--- geradorDeCalendario.php
+++ geradorDeCalendario.php
@@ -1,40 +1,28 @@
 <?php
 
 if (isset($_POST['DATAFIN'])) {
+    include './gerenciadorDeFuncoes.php';
  $servidor = "52.34.253.148";
 $usuario = "pe";
 $banco = "DBSystemSec";
 $senha = "!@#123qwe";
 //NÃ£o Alterar abaixo:
-$conmssql = mssql_connect($servidor.":1433",$usuario,$senha);
+$conmssql = mssql_connect($servidor,$usuario,$senha);
 $db = mssql_select_db($banco, $conmssql);
 if ($conmssql && $db){
 echo "Parabens!! A conexÃ£o ao banco de dados ocorreu normalmente!";
 } else {
 echo "Nao foi possivel conectar ao banco MSSQL";
 }
-
- 
-
-    
-   echo $dataInicial = $_POST['DATAINI'];
-   echo $dataFinal = $_POST['DATAFIN'];
+   $dataInicial = $_POST['DATAINI'];
+   $dataFinal = $_POST['DATAFIN'];
    
-   
-   echo "teste2";
-   
-   
-   $arrayHoraIni = array();
-   $arrayHoraFini = array();
-   $arrayAula = array();
-   $arrayCodigoTurno = array();
-
    //$horaPulo = 0;
    //$horaAnterior = 0;
     for ($i = strtotime($dataInicial); $i <= strtotime($dataFinal); $i = $i + 86400) {
         //echo "teste";
       $i;
-        echo "teste";
+      
       $datai = date("d-m-Y", $i);
       $datai2 = date("Y-m-d", $i);
 
@@ -64,26 +52,26 @@ echo "Nao foi possivel conectar ao banco MSSQL";
 
         switch ($diaSemana) {
             case "Seg":
-               echo $sql = "select * from PHE_SHORARIO h where h.CODTURNO = 1 and h.DIASEMANA = 2;";
-                $result = mysql_query($sql);
-                if ($result) {
-                    while ($linha = mysql_fetch_array($result)) {
-                        $arrayHoraIni[] = $linha['HORINI'];
-                        $arrayHoraFini[] = $linha['HORFIM'];
-                        $arrayAula[] = $linha['AULA'];
-                        $arrayCodigoTurno[] = $linha['CODTURNO'];
-                    }
-                    for ($index = 0; $index < count($arrayCodigoTurno);$index++) {
-                    echo   $sql = "INSERT INTO PHE_CALENDARIO_ESCOLA (DATA,HORINI,HORFIM,THORAAULA,CODTURNO,CODFILIAL,CODLETIVO,STATUS,DIASSEMANA) "
-                               . "VALUES ('$datai2',$arrayHoraIni[$index]','$arrayHoraFini[$index]','01:00:00.0000000')','$arrayCodigoTurno[$index]',3,1,'A','SEGUNDA')";
-                    }   
-                   
-                }
+                insereHorario(2,$datai2);
                 break;
-
+            case "Ter":
+                insereHorario(3,$datai2);
+                break;
+            case "Qua":
+                insereHorario(4,$datai2);
+                break;
+            case "Qui":
+                insereHorario(5,$datai2);
+                break;
+            case "Sex":
+                insereHorario(6,$datai2);
+                break;
+            case "Sab":
+                insereHorario(7,$datai2);
+                break;
             default:
                 break;
         }
     }
-}
 
+}
--- gerenciadorDeFuncoes.php
+++ gerenciadorDeFuncoes.php
@@ -1,4 +1,25 @@
 <?php
-
-
-
+function insereHorario($diaDaSemana,$data) {
+    $arrayHoraIni = array();
+    $arrayHoraFini = array();
+    $arrayAula = array();
+    $arrayCodigoTurno = array();
+    $sql = "SELECT * FROM PHE_SHORARIO H WHERE H.DIASEMANA = $diaDaSemana
+            ORDER BY H.DIASEMANA
+            OFFSET 0 ROWS FETCH NEXT 20 ROWS ONLY";
+    $result = mssql_query($sql);
+    if ($result) {
+        while ($linha = mssql_fetch_array($result)) {
+            $arrayHoraIni[] = $linha['HORAINICIAL'];
+            $arrayHoraFini[] = $linha['HORAFINAL'];
+            $arrayAula[] = $linha['AULA'];
+            $arrayCodigoTurno[] = $linha['CODTURNO'];
+        }
+        for ($index = 0; $index < count($arrayCodigoTurno); $index++) {
+             $insert = "INSERT dbo.PHE_CALENDARIO_ESCOLA (DATA,HORINI,HORFIM,THORAAULA,CODTURNO,CODFILIAL,CODLETIVO,STATUS,DIASEMANA)"
+            . "VALUES ('$data','$arrayHoraIni[$index]','$arrayHoraFini[$index]','01:00:00.0000000',$arrayCodigoTurno[$index],3,1,'A','SEGUNDA')";
+            $result = mssql_query($insert);
+        }
+        echo "deu certo";
+    }
+}
