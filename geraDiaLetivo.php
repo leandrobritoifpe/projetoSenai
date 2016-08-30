5b47ab0e9476caa38d7a6d7db271fcaa50a13e80
Ets.areias <Ets.areias>
2016-08-30 12:11:51 -0300

    o programa já sabe gera dias letivos

---
+++ geraDiaLetivo.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * CLASSE DiaNaoLetivoDao
+ * OJETIVO : RESPOSAVEL POR TODA A COMUNICACAO COM O BANCO DE DADOS
+ * CRIADA : 25/08/2016
+ * ULTIMA ATUALIZACAO : 30/08/2016
+ * 
+ * DS -> LEANDRO BRITO ;)
+ */
+
+include_once './entidades/CalendarioEscolar.php';
+include_once './dao/CalendarioEscolarDao.php';
+include './gerenciadorDeFuncoes.php';
+
+$dao = new CalendarioEscolarDao();
+$dao->abrirConexao();
+$mensagem = exibeMesagensParaUsuario($dao->geraDiaLetivo());
+$dao->fechaBanco();
+echo "<script>window.location='index.php';alert('$mensagem');</script>";
+
+
